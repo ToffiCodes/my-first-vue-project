724b72dab888827463256683aebc6bb18e34eb19
ToffiCodes <tevfik.kohl@gmail.com>
2024-04-08 16:49:42 +0200

    added router

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <LayoutHeader />
-  <main>
-    <div id="app" class="inside">
-      <BookList />
-    </div>
-  </main>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<script>
-import LayoutHeader from "@/components/LayoutHeader.vue";
-import BookList from "@/components/BookList.vue";
-
-export default {
-  name: "App",
-  components: {
-    LayoutHeader,
-    BookList,
-  },
-};
-</script>
-
 <style>
-@import url("@/assets/reset.css");
-@import url("@/assets/global.css");
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
---
+++ src/AppCopy.vue
@@ -0,0 +1,26 @@
+<template>
+  <LayoutHeader />
+  <main>
+    <div id="app" class="inside">
+      <BookList />
+    </div>
+  </main>
+</template>
+
+<script>
+import LayoutHeader from "@/components/LayoutHeader.vue";
+import BookList from "@/components/BookList.vue";
+
+export default {
+  name: "App",
+  components: {
+    LayoutHeader,
+    BookList,
+  },
+};
+</script>
+
+<style>
+@import url("@/assets/reset.css");
+@import url("@/assets/global.css");
+</style>
